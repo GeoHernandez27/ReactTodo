05405fba8c8ed90fa834a480b85c9b0639287d42
GeoHernandez27 <leopardo2@gmail.com>
2020-02-27 15:03:06 -0700

    cleaned up template files. Started list functionality, added completed toggle

---
+++ src/components/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+    markComplete=(e) => {
+        console.log(this.props);
+        
+    }
+
+    render(){
+
+        
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} />
+        ));
+    }
+}
+// PropTypes
+Todos.propTypes = {
+    todos: PropTypes.object.isRequired
+}
+
+export default Todos;
