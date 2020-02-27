b496806353f254e90860376ec32dfba51c5ccbc2
GeoHernandez27 <leopardo2@gmail.com>
2020-02-27 15:20:37 -0700

    added delete functionality, added AddTodo bar, cleaned up styling

--- src/App.js
+++ src/App.js
@@ -1,52 +1,65 @@
-import React, { Component } from 'react';
-import Todos from './components/Todos'
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddToDo";
 
-import './App.css';
-
+import "./App.css";
 
 class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        title: 'Wash the car',
+        title: "Wash the car",
         completed: false
       },
       {
         id: 2,
-        title: 'Clean Dishes',
-        completed: true
+        title: "Clean Dishes",
+        completed: false
       },
       {
         id: 3,
-        title: 'Mow the lawn',
+        title: "Mow the lawn",
         completed: false
       }
     ]
-  }
+  };
 
   // Toggle Complete
-  markComplete=(id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id){
-        todo.completed = !todo.completed
-      }
-      return todo;
-    })})
-    
-}
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
 
+  // Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
 
-render(){
-
-  
-  
-  return (
-    <div >
-      <Todos todos={this.state.todos} markComplete={this.markComplete}/>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
