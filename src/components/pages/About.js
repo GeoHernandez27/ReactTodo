939ec6f2eac8d5d630053bb00d8725525744b9cc
GeoHernandez27 <leopardo2@gmail.com>
2020-02-28 09:32:27 -0700

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the Todo List.</p>
+            <p>To do list built using React and React-Router. API calls to jsonplaceholder made with axios.</p>
         </React.Fragment>
     )
 }
